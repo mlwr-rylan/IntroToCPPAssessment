bc31eaeffb551c7b6a4ad0f57bc7cace9c225bb5
Ethan Gandy <gandyethan4@gmail.com>
2023-12-20 16:43:22 -0600

    Added the input class and the three images that we will be using

---
+++ raygame/Input.cpp
@@ -0,0 +1,26 @@
+
+#include "raylib.h"
+#include "Input.h"
+
+void Input::start()
+{
+}
+
+void Input::draw()
+{
+}
+
+void Input::update(float deltaTime)
+{
+
+
+	Vector2 direction = Vector2();
+
+	if (IsKeyPressed(KEY_D))
+	{
+		direction = (1, 0);
+	}
+
+
+
+}
--- raygame/Input.h
+++ raygame/Input.h
@@ -1,6 +1,14 @@
 #pragma once
+#include "Scene.h"
 
 class Input
 {
+public:
+	
+	void start();
+
+	void draw();
+
+	void update(float deltaTime);
 };
 
